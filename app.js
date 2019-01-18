190aec0d2f1d35c0377d98cb5377ddd88691eb91
Rob Baile <robbaile12@outlook.com>
2019-01-18 14:58:35 +0000

    changed app.js to render messages and added css styles

--- app.js
+++ app.js
@@ -2,19 +2,34 @@ var db = new PouchDB('rob_database');
 
 console.log(db);
 
-var doc = {
-    "_id": "messages",
-    "messages": [
-      "I'm a hard coded message in the array",
-      "I'm another hard coded message in the array",
-      "I'm the last message and I'm rather long to make sure that we style the page correctly so that it looks good with long and short messages"
-    ]
-};
+  db.get('messages').then(function(doc) {
+        renderMessages(doc);
+    }).catch(function(e) {
+        console.log(e)
+        if (e.message == 'missing') {
+            // we do everything here
+            db.put({
+                "_id": "messages",
+                "messages": [
+                "I'm a hard coded message in the array",
+                "I'm another hard coded message in the array",
+                "I'm the last message and I'm rather long to make sure that we style the page correctly so that it looks good with long and short messages"
+                ]
+            }).then(function(response) {
+                db.get('messages').then(function(doc) {
+                    renderMessages(doc)
+                })
+            })
+        }
+    })
 
-
-  db.get('messages').then((doc) => {
-    console.log(doc);
-  });
+  function renderMessages(doc) {
+      console.log('rendering messages with doc:')
+      console.log(doc);
+      const formattedMessages = doc.messages.map(message => `<p class="message-p">${message}</p>`).join('');
+      const messageContainer = document.querySelector('.messages-container');
+      messageContainer.innerHTML = formattedMessages;
+  }
 
 
 let form = document.querySelector('.message-form');
@@ -26,10 +41,13 @@ form.addEventListener('submit', (e) => {
     textInput.value = ""
     
 
-    db.get('messages').then((doc) => {
+    db.get('messages').then(function(doc) {
         doc.messages.push(message);
+        renderMessages(doc);
         return db.put(doc);
-    }).catch(err => {
+    }).catch(function(err) {
         console.log(err);
     })
 })
+
+db.replicate.to('https://db-server.robbaile.me');
